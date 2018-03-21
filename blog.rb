1798ec782286e6124b60de7a4b55eacd8fbfc93d
fuzisan <toshichanapp@gmail.com>
2018-03-21 12:42:45 +0000

    section13

--- blog.rb
+++ blog.rb
@@ -3,5 +3,25 @@ puts '1:ブログを作成する'
 puts '2:作成されたブログを見る'
 puts '3:ブログアプリを終了する'
 
-number = gets
-puts number
+number = gets.to_i
+
+if number == 1
+  puts '1:ブログを作成する'
+
+  puts 'ブログのタイトルを入力してください'
+  blog_title = gets
+
+  puts 'ブログの本文を入力してください'
+  blog_content = gets
+
+  puts "入力されたタイトルと本文は以下です。"
+
+  puts "タイトル：#{blog_title}"
+  puts "本文：#{blog_content}"
+elsif number == 2
+  puts '2:作成されたブログを見る'
+elsif number == 3
+  puts '3:ブログアプリを終了する'
+else
+  puts '1~3の数字を入力してください'
+end
