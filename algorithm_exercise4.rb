706c2a8f844fa7ff3e2a1a8bd8b3f831dcaaaf09
fuzisan <toshichanapp@gmail.com>
2018-03-22 13:20:23 +0000

    repeat_string

---
+++ algorithm_exercise4.rb
@@ -0,0 +1,10 @@
+
+def repeat_string(str)
+  spliced_str = str.slice(0, 4)
+  puts spliced_str * 3
+end
+
+repeat_string('Python')
+repeat_string('Go')
+repeat_string('C++')
+repeat_string('JavaScript')
