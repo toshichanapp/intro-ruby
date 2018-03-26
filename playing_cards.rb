ea323cd821b6ea9b6768af1c200ee8b4d16f1a73
fuzisan <toshichanapp@gmail.com>
2018-03-26 12:25:56 +0000

    task add quick sort

--- playing_cards.rb
+++ playing_cards.rb
@@ -24,6 +24,22 @@ target = hand.to_a
 
 # FIXME （targetを昇順にソートしてください）
 
+def quick_sort(sort_number)
+  return sort_number if sort_number.size < 2
+  target = sort_number.shift
+  smallers = []
+  biggers = []
+  sort_number.each do |sort_numbers|
+      if sort_numbers < target
+          smallers << sort_numbers
+      else
+          biggers << sort_numbers
+      end
+  end
+  quick_sort(smallers) + [target] + quick_sort(biggers)
+end
+
+target = quick_sort(target)
 # hand = hand.sort!は使用しないでください。
 # 並び替えた配列でHandオブジェクトのインスタンスを作成
 hand = Hand.new(target)
